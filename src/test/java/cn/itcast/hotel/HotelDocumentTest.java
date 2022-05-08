9efc86cb5772e39d706bf60de201771ae69ca93a
MoMO <br0RHQy@$Ipo>
2022-05-08 20:14:26 +0800

    文档crud结束

--- src/test/java/cn/itcast/hotel/HotelDocumentTest.java
+++ src/test/java/cn/itcast/hotel/HotelDocumentTest.java
@@ -6,9 +6,12 @@ import cn.itcast.hotel.service.IHotelService;
 import com.alibaba.fastjson.JSON;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -22,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.List;
 
 import static cn.itcast.hotel.constants.HotelConstants.MAPPING_TEMPLATE;
 
@@ -55,6 +59,45 @@ public class HotelDocumentTest {
         String json = response.getSourceAsString();
         HotelDoc hotelDoc = JSON.parseObject(json, HotelDoc.class);
         System.out.println(hotelDoc);
+    }
+
+    @Test
+    void testUpdateDocument() throws IOException {
+        //1.准备Request
+        UpdateRequest request = new UpdateRequest("hotel", "36934");
+        //2.准备请求参数
+        request.doc(
+                "price","9527",
+                "starName","三钻"
+        );
+        //3.发送请求
+        client.update(request,RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testDeleteDocument() throws IOException {
+        //1.准备Request
+        DeleteRequest request = new DeleteRequest("hotel", "36934");
+        //2.发送请求
+        client.delete(request,RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testBulkRequest() throws IOException {
+        //批量查询酒店数据
+        List<Hotel> hotles = hotelService.list();
+        //1.准备Request
+        BulkRequest request = new BulkRequest();
+        for (Hotel hotle : hotles) {
+            //转换为文档类型
+            HotelDoc hotelDoc = new HotelDoc(hotle);
+            //2.准备参数，添加多个新增的request
+            request.add(new IndexRequest("hotel")
+                    .id(hotelDoc.getId().toString())
+                    .source(JSON.toJSONString(hotelDoc),XContentType.JSON));
+        }
+        //3.发送请求
+        client.bulk(request,RequestOptions.DEFAULT);
     }
 
     @BeforeEach
